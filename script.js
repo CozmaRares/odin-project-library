fb2a9132bd3e566ef959fa69b3fca20d48526a80
Cozma Rares <cozmarares11@gmail.com>
2022-09-01 14:48:36 +0300

    add header funcionality

--- script.js
+++ script.js
@@ -11,6 +11,46 @@ function loadBooks(booksData) {
     else books[book.status].push(newBook);
   });
 
+  displayAllBooks();
+}
+
+fetch("./books.json")
+  .then(response => {
+    if (response.ok) return response.json();
+  })
+  .then(loadBooks);
+
+function filterBooks(headerLI) {
+  const statusClass = headerLI.className
+      .replace("selected", "")
+      .replaceAll(" ", ""),
+    status = statusClass.replaceAll("-", " ");
+
+  [...document.querySelectorAll("header li")].forEach(li =>
+    li.classList.remove("selected")
+  );
+
+  headerLI.classList.toggle("selected");
+
+  refreshGrid();
+  if (statusClass === "") return displayAllBooks();
+
+  displayBooksByStatus(status, statusClass);
+}
+
+function refreshGrid() {
+  document.querySelector(".grid").innerHTML = `
+    <div class="line"></div>
+    <div class="index">#</div>
+    <div class="border-l">Title</div>
+    <div class="border-l">Author</div>
+    <div class="pages border-l">Pages</div>
+  `;
+}
+
+function displayAllBooks() {
+  document.querySelector(".title").innerText = "All Books";
+
   const grid = document.querySelector(".grid");
 
   const statusOrder = [
@@ -26,37 +66,35 @@ function loadBooks(booksData) {
   statusOrder.forEach(status => {
     const statusClass = status.split(" ").join("-");
 
-    books[status]
-      .sort((a, b) => {
-        let ta = a.title.toLowerCase(),
-          tb = b.title.toLowerCase();
-
-        if (ta < tb) {
-          return -1;
-        }
-        if (ta > tb) {
-          return 1;
-        }
-        return 0;
-      })
-      .forEach(({ title, author, pages }) => {
-        grid.insertAdjacentHTML(
-          "beforeend",
-          `<div class="line ${statusClass}"></div>`
-        );
-        grid.insertAdjacentHTML(
-          "beforeend",
-          `<div class="index">${idx++}</div>`
-        );
-        grid.insertAdjacentHTML("beforeend", `<div>${title}</div>`);
-        grid.insertAdjacentHTML("beforeend", `<div>${author}</div>`);
-        grid.insertAdjacentHTML("beforeend", `<div>${pages}</div>`);
-      });
+    books[status].forEach(({ title, author, pages }) => {
+      grid.insertAdjacentHTML(
+        "beforeend",
+        `<div class="line ${statusClass}"></div>`
+      );
+      grid.insertAdjacentHTML("beforeend", `<div class="index">${idx++}</div>`);
+      grid.insertAdjacentHTML("beforeend", `<div>${title}</div>`);
+      grid.insertAdjacentHTML("beforeend", `<div>${author}</div>`);
+      grid.insertAdjacentHTML("beforeend", `<div class="pages">${pages}</div>`);
+    });
   });
 }
 
-fetch("./books.json")
-  .then(response => {
-    if (response.ok) return response.json();
-  })
-  .then(loadBooks);
+function displayBooksByStatus(status, statusClass) {
+  document.querySelector(".title").innerText = status;
+
+  const grid = document.querySelector(".grid");
+
+  let idx = 1;
+
+  debugger;
+  books[status].forEach(({ title, author, pages }) => {
+    grid.insertAdjacentHTML(
+      "beforeend",
+      `<div class="line ${statusClass}"></div>`
+    );
+    grid.insertAdjacentHTML("beforeend", `<div class="index">${idx++}</div>`);
+    grid.insertAdjacentHTML("beforeend", `<div>${title}</div>`);
+    grid.insertAdjacentHTML("beforeend", `<div>${author}</div>`);
+    grid.insertAdjacentHTML("beforeend", `<div class="pages">${pages}</div>`);
+  });
+}
